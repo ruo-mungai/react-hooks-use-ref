0b6350772db2dd74bedefa65580cbf01473ce73e
Patrick <ruomungai@gmail.com>
2022-07-24 10:53:56 +0300

    commit tree 9559f912e647c0fff026b4cf02144230ee29688b

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,16 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
 
+  const prevPriceRef = useRef(price);
+
+  useEffect(() => {
+  
+    const prevPrice = prevPriceRef.current;
+
+    console.log({ price, prevPrice });
+
+    if (price > prevPrice) {
+      setColor("green");
+    } else if (price < prevPrice) {
+      setColor("red");
+    } else {
+      setColor("black");
+    }
+   
+    prevPriceRef.current = price;
+  }, [price]);
+
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
     return function () {
       clearInterval(id);
     };
-  }, []);
+  }, [price]);
 
   return (
     <div>
